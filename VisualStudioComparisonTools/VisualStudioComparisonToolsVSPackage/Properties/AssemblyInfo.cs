e7748e9fcdce9278ca81bc0b51469046335fbc72
Mikko Halttunen <mikko@kovasolutions.com>
2013-10-06 22:27:34 +0300

    A vspackage version of the tool

---
+++ VisualStudioComparisonTools/VisualStudioComparisonToolsVSPackage/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VSComparisonTools")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mikko Halttunen")]
+[assembly: AssemblyProduct("VSComparisonTools")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("VisualStudioComparisonToolsVSPackage_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001005f165cbf137cb0e4fc7f996568e709fbca3ccdfd85d8d9bc8d7c2b4c2b4efac232b69c6c51b3e5eacb00eb2310726bd54a80844a1b688fd798279dcdeea1a09876b0840dc4561402651c28a8e4087b491cd262916acec79ce8553afb8e0c3c7565a572e518e268e9f27d77db3c8ae60bda409c812df2f2390f5a0757e779afa7")]
+[assembly: InternalsVisibleTo("VisualStudioComparisonToolsVSPackage_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001005f165cbf137cb0e4fc7f996568e709fbca3ccdfd85d8d9bc8d7c2b4c2b4efac232b69c6c51b3e5eacb00eb2310726bd54a80844a1b688fd798279dcdeea1a09876b0840dc4561402651c28a8e4087b491cd262916acec79ce8553afb8e0c3c7565a572e518e268e9f27d77db3c8ae60bda409c812df2f2390f5a0757e779afa7")]
